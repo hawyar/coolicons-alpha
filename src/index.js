2e76603d1be706e4dd3d196ea2162ef949033582
Hawyar <hawyarxoshnaw@gmail.com>
2020-09-20 18:15:12 -0400

    fetch node id and name

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
 const svgr = require('@svgr/core').default;
+const fetcher = require('./util/fetcher');
+const fs = require('fs');
 
-const svgCode = `
-<svg xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink">
-  <rect x="10" y="10" height="100" width="100"
-    style="stroke:#ff0000; fill: #0000ff"/>
-</svg>
-`;
+//TODO: get SVG names and id
+// map each svg id to a request for its content
+// generate SVG componenet
+async function generateSVGS() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await fetcher.getFiles().then((e) => {
+        return e;
+      });
 
-svgr(svgCode, { icon: true }, { componentName: 'MyComponent' }).then(
-  (jsCode) => {
-    console.log(jsCode);
-  }
-);
+      if (data.nodes['0:1'].document.type !== 'CANVAS' || !data) {
+        reject('Unable to fetch figma content');
+      }
+
+      const IconCanvas = await data.nodes['0:1'].document;
+      const content = IconCanvas.children.map((el) => {
+        return el.type === 'FRAME';
+      });
+
+      resolve(content);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+generateSVGS().then((data) => {
+  fs.writeFile('./output.json', JSON.stringify(data), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
+
+// // this is equivalent to a page in figma
+
+// data.nodes['0:1'].document.children[0].children.map((el) => {
+//   console.log(el.name);
+// });
+
+// // for (const target of Object.entries(
+// //   data.nodes['0:1'].document.children[0].children[0]
+// // )) {
+// //   console.log(target);
+// // }
+// return data;
--- src/util/fetcher.js
+++ src/util/fetcher.js
@@ -1,16 +1,27 @@
-const fetch = require('node-fetch');
-const { token } = require('../config/reources');
-export async function getPages(url) {
-  const config = {
-    method: 'GET',
-    headers: {
-      'X_FIGMA-Token': token,
-    },
-  };
-  await fetch(url, config).then((data) => {
-    console.log(data);
-    return data.json();
-  });
-}
+const { file, token } = require('../util/resources');
 
 //path.join(base, "/files", resource.file, "?depth=1"
+
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: `https://api.figma.com/v1/files/${file}/nodes?ids=0%3A1`,
+  headers: {
+    'X-FIGMA-TOKEN': ` ${token}`,
+  },
+};
+async function getFiles() {
+  const figma = await axios(config)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return figma;
+}
+
+module.exports = {
+  getFiles,
+};
